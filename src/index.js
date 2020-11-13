53e3115ca3f7929874ecc0614bca1039eedc2f8c
Charlie Howard <charliehoward@charlies-mbp.home>
2020-11-13 17:45:30 +0000

    added initaliseboard function

--- src/index.js
+++ src/index.js
@@ -19,8 +19,40 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      boardData: this.initialiseBoard(
+        this.props.height,
+        this.props.width,
+        this.props.mines
+      ),
+      gameWon: false,
     };
+  }
+
+  initialiseBoard(height, width, mines) {
+    let boardData = [];
+
+    for (let i = 0; i < height; i++) {
+      boardData.push([]);
+      for (let j = 0; j < width; j++) {
+        boardData[i][j] = {
+          x: i,
+          y: j,
+          isMine: false,
+          neighbouringMines: 0,
+          isFlagged: false,
+          isRevealed: false,
+        };
+      }
+    }
+
+    // set the mines in the board
+    boardData = this.setMines(boardData, mines);
+
+    // set the value for non-mine cells, how many neighbouring mines?
+    boardData = this.findNeighbouringMines(boardData);
+
+    // return the initalised boardData to Board's state
+    return boardData;
   }
 
   render() {
@@ -32,12 +64,23 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      height: 10,
+      width: 10,
+      mines: 25,
     };
   }
 
   render() {
-    return <div>Game</div>;
+    const { height, width, mines } = this.state;
+    return (
+      <div className="game">
+        <h1>Minesweeper</h1>
+        <p>Number of mines in the board: {mines}</p>
+        <div className="game-board">
+          <Board height={height} width={width} mines={mines} />
+        </div>
+      </div>
+    );
   }
 }
 
