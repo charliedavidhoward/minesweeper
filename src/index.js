ad621573f9d74521a61b6aa3983f66b40cdb3db2
Charlie Howard <charliehoward@charlies-mbp.home>
2020-11-13 19:01:15 +0000

    add findNeighbouringMines and neighbouringCells functions

--- src/index.js
+++ src/index.js
@@ -48,8 +48,9 @@ class Board extends React.Component {
     boardData = this.setMines(boardData, mines);
 
     // set the value for non-mine cells, how many neighbouring mines?
-    // boardData = this.findNeighbouringMines(boardData);
+    boardData = this.findNeighbouringMines(boardData);
 
+    console.log(boardData);
     // return the initalised boardData to Board's state
     return boardData;
   }
@@ -59,12 +60,9 @@ class Board extends React.Component {
     let randomY = 0;
     let minesSet = 0;
 
-    let height = boardData.length;
-    let width = boardData[0].length;
-
     while (minesSet < mines) {
-      randomX = Math.floor(Math.random() * width);
-      randomY = Math.floor(Math.random() * height);
+      randomX = Math.floor(Math.random() * this.props.width);
+      randomY = Math.floor(Math.random() * this.props.height);
 
       if (!boardData[randomY][randomX].isMine) {
         boardData[randomY][randomX].isMine = true;
@@ -72,6 +70,80 @@ class Board extends React.Component {
       }
     }
     return boardData;
+  }
+
+  findNeighbouringMines(boardData) {
+    for (let i = 0; i < this.props.height; i++) {
+      for (let j = 0; j < this.props.width; j++) {
+        if (!boardData[i][j].isMine) {
+          let neighbouringMines = 0;
+          const neighbouringCells = this.neighbouringCells(
+            boardData[i][j].x,
+            boardData[i][j].y,
+            boardData
+          );
+
+          for (let x = 0; x < neighbouringCells.length; x++) {
+            if (neighbouringCells[x].isMine) {
+              neighbouringMines++;
+            }
+          }
+
+          boardData[i][j].neighbouringMines = neighbouringMines;
+        }
+      }
+    }
+
+    return boardData;
+  }
+
+  neighbouringCells(x, y, boardData) {
+    let cells = [];
+
+    const height = this.props.height;
+    const width = this.props.width;
+
+    // to the left
+    if (x > 0) {
+      cells.push(boardData[x - 1][y]);
+    }
+
+    // to the top-left
+    if (x > 0 && y > 0) {
+      cells.push(boardData[x - 1][y - 1]);
+    }
+
+    // to the top
+    if (y > 0) {
+      cells.push(boardData[x][y - 1]);
+    }
+
+    // to the top-right
+    if (x < width - 1 && y > 0) {
+      cells.push(boardData[x + 1][y - 1]);
+    }
+
+    // to the right
+    if (x < width - 1) {
+      cells.push(boardData[x + 1][y]);
+    }
+
+    // to the bottom-right
+    if (x < width - 1 && y < height - 1) {
+      cells.push(boardData[x + 1][y + 1]);
+    }
+
+    // to the bottom
+    if (y < height - 1) {
+      cells.push(boardData[x][y + 1]);
+    }
+
+    // to the bottom-left
+    if (x > 0 && y < height - 1) {
+      cells.push(boardData[x - 1][y + 1]);
+    }
+
+    return cells;
   }
 
   render() {
@@ -84,7 +156,7 @@ class Game extends React.Component {
     super(props);
     this.state = {
       height: 20,
-      width: 10,
+      width: 20,
       mines: 25,
     };
   }
